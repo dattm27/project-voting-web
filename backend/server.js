24ddd4312b7ae6e0bbd175c10a0a68106fa48b1f
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-10-25 03:44:38 +0700

    add nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+//const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 const app = express();
 
 app.use(express.json());
 
 // Sử dụng các routes
-app.use('/users', userRoutes);
+//app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
