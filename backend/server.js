f6ca62c351263dbe26184f88f22d9339177f23db
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-11-01 10:36:13 +0700

    add env to connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const pool = require('./config/database');
 require('dotenv').config();
 const app = express();
 
@@ -9,6 +10,15 @@ app.use(express.json());
 app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connected to the database');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
