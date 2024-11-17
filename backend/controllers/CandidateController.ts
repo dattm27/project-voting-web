3b5deda6a149b8e8a9addd2251e7f1eac79fc118
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-11-17 14:16:53 +0700

    update

--- backend/controllers/CandidateController.ts
+++ backend/controllers/CandidateController.ts
@@ -27,7 +27,11 @@ export const createCandidate = async (req: Request, res: Response) : Promise<voi
 // Get all candidates
 export const getCandidates = async (req: Request, res: Response) => {
     try {
-        const candidates = await candidateRepository.find();
+        const candidates = await candidateRepository.find(
+            {
+                relations: ['photo'],
+            }
+        );
         res.status(200).json(candidates);
     } catch (error) {
         console.error('ERROR getting candidates', error);
@@ -38,7 +42,13 @@ export const getCandidates = async (req: Request, res: Response) => {
 // Get a candidate by ID
 export const getCandidateById = async (req: Request, res: Response) => {
     try {
-        const candidate = await candidateRepository.findOne(req.body.id);
+        const id = req.params.id;
+        const candidate = await candidateRepository.findOne(
+            {
+                where: { id: parseInt(id, 10) },
+                relations: ['photo'],
+            }
+        );
         if (!candidate) {
             res.status(404).json({ error: 'Candidate not found' });
         } else {
@@ -51,32 +61,55 @@ export const getCandidateById = async (req: Request, res: Response) => {
 };
 
 // Update a candidate by ID
-export const updateCandidate = async (req: Request, res: Response) => {
+export const updateCandidate = async (req: Request, res: Response) : Promise<void> => {
     try {
-        const candidate = await candidateRepository.findOne(req.body.id);
+        const candidate = await candidateRepository.findOne({ where: { id: req.body.id }, relations: ['photo'] });
         if (!candidate) {
             res.status(404).json({ error: 'Candidate not found' });
-        } else {
-            candidateRepository.merge(candidate, req.body);
-            await candidateRepository.save(candidate);
-            res.status(200).json(candidate);
         }
+
+        // Kiểm tra xem có thông tin Photo mới trong yêu cầu hay không
+        const { photoBase64 } = req.body;
+        const { photoLink, photoDescription } = photoBase64 ? await dropBoxServices.convertBase64ToFile(photoBase64) : {};
+        if (photoLink && photoDescription) {
+            // Xóa Photo cũ nếu có
+            if (candidate.photo) {
+                candidate.photo.link = photoLink;
+                candidate.photo.description = photoDescription;
+                await photoRepository.save(candidate.photo);
+            } else {
+                // Tạo và lưu Photo mới nếu không có Photo hiện tại
+                const newPhoto = new Photo(photoLink, photoDescription);
+                const savedPhoto = await photoRepository.save(newPhoto);
+                candidate.photo = savedPhoto;
+            }
+        }
+
+        // Cập nhật các trường khác của Candidate
+        candidateRepository.merge(candidate, req.body);
+        const updatedCandidate = await candidateRepository.save(candidate);
+        res.status(200).json(updatedCandidate);
     } catch (error) {
         console.error('ERROR updating candidate', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
-
 // Delete a candidate by ID
-export const deleteCandidate = async (req: Request, res: Response) => {
+export const deleteCandidate = async (req: Request, res: Response) : Promise<void> => {
     try {
-        const candidate = await candidateRepository.findOne(req.body.id);
+        const candidate = await candidateRepository.findOne({ where: { id: req.body.id }, relations: ['photo'] });
         if (!candidate) {
             res.status(404).json({ error: 'Candidate not found' });
-        } else {
-            await candidateRepository.delete(candidate);
-            res.status(204).end();
         }
+
+        // Xóa Photo nếu có
+        if (candidate.photo) {
+            await photoRepository.remove(candidate.photo);
+        }
+
+        // Xóa Candidate
+        await candidateRepository.remove(candidate);
+        res.status(204).end();
     } catch (error) {
         console.error('ERROR deleting candidate', error);
         res.status(500).json({ error: 'Internal Server Error' });
