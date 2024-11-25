848d4061affa71aa950ca08765f0a4e350172539
LeTuanDat_Maris <datlt@falcongames.com>
2024-11-25 13:27:15 +0700

    change endpoint

--- frontend/src/Services/endPointConfig.js
+++ frontend/src/Services/endPointConfig.js
@@ -1,5 +1,6 @@
 const endPointConfig = {
-    serverIP: 'http://ec2-54-153-162-131.ap-southeast-2.compute.amazonaws.com:3000',
+    //serverIP: 'http://ec2-54-153-162-131.ap-southeast-2.compute.amazonaws.com:3000',
+    serverIP: 'http://localhost:3000',
     apiBaseURL: '/api/',
     endpoints: {
         elections: 'elections',
