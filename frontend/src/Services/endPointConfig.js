5397dfc407d217c4822d807c4912a2faddea9836
dat94-03 <tiendat942003@gmail.com>
2024-12-07 12:14:49 +0700

    replace enpoint BE

--- frontend/src/Services/endPointConfig.js
+++ frontend/src/Services/endPointConfig.js
@@ -1,5 +1,5 @@
 const endPointConfig = {
-    serverIP: 'http://ec2-54-153-162-131.ap-southeast-2.compute.amazonaws.com:3000',
+    serverIP: 'https://project-voting-web.onrender.com',
     //serverIP: 'http://localhost:3000',
     apiBaseURL: '/api/',
     endpoints: {
