cc75545b72be4a2f3072faf5a6c3a312c46680b2
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-12-14 23:46:42 +0700

    update candidate authentication

--- backend/controllers/CandidateController.ts
+++ backend/controllers/CandidateController.ts
@@ -2,17 +2,30 @@ import { Request, Response } from 'express';
 import { Candidate } from '../models/Candidate';
 import AppDataSource from '../config/database';
 import { CloudinaryServices } from '../services/CloudinaryServices';
-import {CustomRequest} from '../middleware/auth';
+import { CustomRequest } from '../middleware/auth';
+import Election from '../models/Election';
 
 const candidateRepository = AppDataSource.getRepository(Candidate);
+const electionRepository = AppDataSource.getRepository(Election);
 
 // Create a new candidate
-export const createCandidate = async (req: Request, res: Response) : Promise<void> => {
+export const createCandidate = async (req: Request, res: Response): Promise<void> => {
     try {
-        const {id, name, birthDay, description, roll, votes, electionId, photoLink} = req.body;
-        const candidate = new Candidate(id, name, new Date(birthDay), description, roll, votes, electionId, photoLink);
-        const savedCandidate = await candidateRepository.save(candidate);
-        res.status(201).json(savedCandidate);
+        const { walletAddress } = req.body;
+        if (!walletAddress) {
+            res.status(400).json({ message: 'Wallet address is required' });
+            return;
+        }
+        const { id, name, birthDay, description, roll, votes, electionId, photoLink } = req.body;
+        electionRepository.findOne({ where: { id: electionId, walletAddress: walletAddress } }).then(async (election) => {
+            if (!election) {
+                res.status(404).json({ error: 'missing authorize' });
+                return;
+            }
+            const candidate = new Candidate(id, name, new Date(birthDay), description, roll, votes, electionId, photoLink);
+            const savedCandidate = await candidateRepository.save(candidate);
+            res.status(201).json(savedCandidate);
+        });
     } catch (error) {
         console.error('ERROR creating candidate', error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -39,7 +52,7 @@ export const getDetailCandidate = async (req: Request, res: Response) => {
         const id = req.params.id;
         const electionId = req.params.electionId;
         const candidate = await candidateRepository.findOne({
-            where: { id: parseInt(id, 10) , electionId: parseInt(electionId, 10) },
+            where: { id: parseInt(id, 10), electionId: parseInt(electionId, 10) },
         });
         if (!candidate) {
             res.status(404).json({ error: 'Candidate not found' });
@@ -55,9 +68,19 @@ export const getDetailCandidate = async (req: Request, res: Response) => {
 // Update a candidate by ID
 export const updateCandidate = async (req: Request, res: Response): Promise<void> => {
     try {
+        const {walletAddress} = req.body;
+        if (!walletAddress) {
+            res.status(400).json({ message: 'Wallet address is required' });
+            return;
+        }
         const id = req.params.id;
         const electionId = req.params.electionId;
-        const candidate = await candidateRepository.findOne({ 
+        const election = await electionRepository.findOne({ where: { id: parseInt(electionId, 10), walletAddress: walletAddress } });
+        if (!election) {
+            res.status(404).json({ error: 'missing authorize' });
+            return;
+        }
+        const candidate = await candidateRepository.findOne({
             where: { id: parseInt(id), electionId: parseInt(electionId) },
         });
         if (!candidate) {
@@ -78,10 +101,20 @@ export const updateCandidate = async (req: Request, res: Response): Promise<void
 // Delete a candidate by ID
 export const deleteCandidate = async (req: Request, res: Response): Promise<void> => {
     try {
+        const { walletAddress } = req.body;
+        if (!walletAddress) {
+            res.status(400).json({ message: 'Wallet address is required' });
+            return;
+        }
         const id = req.params.id;
         const electionId = req.params.electionId;
-        const candidate = await candidateRepository.findOne({ 
-            where: { id: parseInt( id) , electionId: parseInt(electionId)},  
+        const election = await electionRepository.findOne({ where: { id: parseInt(electionId, 10), walletAddress: walletAddress } });
+        if (!election) {
+            res.status(404).json({ error: 'missing authorize' });
+            return;
+        }
+        const candidate = await candidateRepository.findOne({
+            where: { id: parseInt(id), electionId: parseInt(electionId) },
         });
         if (!candidate) {
             res.status(404).json({ error: 'Candidate not found' });
