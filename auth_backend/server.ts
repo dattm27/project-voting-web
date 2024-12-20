02acfb634209793138c56430c460fa572520e869
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-12-20 16:26:45 +0700

    test fix cookies domain

--- auth_backend/server.ts
+++ auth_backend/server.ts
@@ -60,7 +60,7 @@ app.post("/login", async (req, res) => {
 		res.cookie("jwt", jwt, 
 			{
 				httpOnly: true,
-				domain: `https://project-voting-web.onrender.com`,
+				domain: `.onrender.com`,
 				secure: true,
 				sameSite: "none",}
 		);
@@ -90,7 +90,7 @@ app.get("/isLoggedIn", async (req, res) => {
 app.post("/logout", (req, res) => {
 	res.clearCookie("jwt", {
         httpOnly: true,
-		domain: `https://project-voting-web.onrender.com`,
+		domain: `.onrender.com`,
         secure: true,
         sameSite: "none",
     });
